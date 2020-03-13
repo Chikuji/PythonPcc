ba8012403b0bcc713c8ec7ee8f79864892c2bd97
Chikuji <fkc07@live.com>
2020-03-13 20:31:18 -0300

    Aula de python dia 13/04

---
+++ aula.py
@@ -0,0 +1,15 @@
+
+print("Digite o numero 1: ")
+n1 = int(input())
+print("Digite o numero 2: ")
+n2 = int(input())
+print("Digite o numero 3: ")
+n3 = int(input())
+
+
+if (n1 >= n2):
+    print("n1>=n2")
+else:
+    print("n1<n2")
+    
+print("n1 == n2")
